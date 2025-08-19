fdd1a1971cd8f4065c9b52daf57a6265c4cf45f2
Grupo Anchieta <anchieta@net.anchieta.br>
2025-08-18 21:20:28 -0300

    Comandos executados na aula dia 18.08

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+let alunos = [
+    { id: 1, nome: "João"}
+]
+
+app.get('/aluno', (req, res) => {
+    res.json(alunos)
+})
+
+app.post('/aluno', (req, res) => {
+    const novoAluno = { id: alunos.length + 1, ...req.body}
+    alunos.push(novoAluno)
+    res.status(201).json(novoAluno)
+})
+
+app.listen(port, () => {
+    console.log(alunos)
+})
