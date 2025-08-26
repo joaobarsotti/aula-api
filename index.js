a52587241bcf0203d7acab8ee65fa157fb8715d1
JOÃO PEDRO BARSOTTI (2301493) <2301493@edu.anchieta.br>
2025-08-25 21:39:30 -0300

    Documentação API - aula 25.08

--- index.js
+++ index.js
@@ -1,12 +1,81 @@
 const express = require("express")
+const swaggerUi = require("swagger-ui-express")
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerOptions = require("./extend/swagger")
+
+
 const app = express()
 const port = 3000
 
+const specs = swaggerJsdoc(swaggerOptions)
+
+
 app.use(express.json())
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Aluno:
+ *              type: object
+ *              required:
+ *                  - id
+ *                  - nome
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      description: Identificador unico do aluno
+ *                      example: 1
+ *                  nome:
+ *                      type: string
+ *                      description: Nome do aluno
+ *                      example: "João"
+ */
+
 let alunos = [
-    { id: 1, nome: "João"}
-]
+    { id: 1, nome: "João" }
+];
+
+/**
+ * @swagger
+ * /aluno:
+ *      get:
+ *          summary: Retorna todos os alunos cadastrados
+ *          tags: [Alunos]
+ *          responses:
+ *              200:
+ *                  description: Lista de Alunos
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: array
+ *                              items:
+ *                                  $ref: '#/components/schemas/Aluno'
+ */
+
+/**
+ * @swagger
+ * /aluno:
+ *      post:
+ *          summary: Criar um novo aluno
+ *          tags: [Alunos]
+ *          requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          nome:
+ *                              type: string
+ *          responses:
+ *              201:
+ *                  description: Aluno criado
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/Aluno'
+ */
 
 app.get('/aluno', (req, res) => {
     res.json(alunos)
@@ -27,24 +96,12 @@ app.put('/aluno/:id', (req, res) => {
     alunos[alunoIndex] = {id: Number(id), ...req.body}
     res.status(200).json(alunos[alunoIndex])
    } else {
-    res.status(404).json({ massage: "Aluno não encontrado"})
+    res.status(404).json({ message: "Aluno não encontrado"})
    }
 })
 
-/*/
-app.delete('/aluno/:id', (req, res) => {
-    const { id } = req.params
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(specs))
 
-    const alunoIndex = alunos.findIndex(a => a.id== id)
-
-    if (alunoIndex > -1) {
-        alunos.splice(alunoIndex, 1)
-        res.status(200).json({ message: "Aluno removido com sucesso."})
-    } else {
-        res.status(404).json({message: "Aluno não encontrado."})
-    }
-})
-*/
 app.listen(port, () => {
-    console.log(alunos)
+    console.log("Servidor Funcionando")
 })
